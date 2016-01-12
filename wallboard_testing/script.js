47350c0253f0ab8e8a4c25f4d55e4d21f364e35a
jjpeet <james@jjpeet.com>
2016-01-12 12:27:19 +0000

    fifth commit

--- wallboard_testing/script.js
+++ wallboard_testing/script.js
@@ -3,39 +3,32 @@ var urlTargets = [
 	'http://www.bom.gov.au/products/IDR503.loop.shtml#skip',
 	];
 
-var loops = 1;
+var loops = 2;
 var secondsPerTab = 10;
-var tabName = [];
+var mSecondsPerTab = secondsPerTab * 1000;
 var win = [];
 
 // FUNCTIONS
 //-------------------------------------------
-function openInNewTab(url) {
-    var newWin = window.open(url);
-	return newWin;
-}// openInNewTab Function
 
-function closeTab(closeWin){
-	closeWin.close();
-}
-
-function setDelay(i) {
-  setTimeout(function(){
-    console.log(i);
-  }, 5000);
-}
 
 //MAIN PROGRAM
 //-------------------------------------------
-for (var i = 0; i < urlTargets.length; i++) {
-	win[i] = window.open(urlTargets[i],'tab' + i);
-	
-}// Open tabs
+var output = '<ul>';
+	$.each(urlTargets, function (key,val) {
+		output += '<li>' + val + '</li>';
+	});
+output += '</ul>';
+$('#update').html(output); //Display URL Targets in list
 
 
+//Tab opener-closer
 for (var i = 0; i < urlTargets.length; i++) {
-    setTimeout(function(x) { return function() { 
-		console.log('Refocus ' +x + ': ' + urlTargets[x]);
+    	setTimeout(function(x) { return function() { 
+		win[x] = window.open(urlTargets[x]);
+		}; }(i), mSecondsPerTab*(i));	// Open tabs
+		
+    	setTimeout(function(x) { return function() { 
 		win[x].close();
-		}; }(i), 10000*(i+1));	// Open tabs
+		}; }(i), mSecondsPerTab*(i+1));	// Close tabs
 }
